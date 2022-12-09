9b4a384824d74f5c6cf76a6e04d5aa6b43be0a40
pavelegorovkrsn <egorovP9009@yandex.ru>
2022-12-10 02:56:44 +0400

    Task2

--- Bot.java
+++ Bot.java
@@ -24,6 +24,12 @@ import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) {
+        FromDigits runDigits = new FromDigits();
+        try {
+            runDigits.digits();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try{
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
@@ -61,6 +67,25 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
+       /* final List<PhotoSize> photo = update.getMessage().getPhoto();
+        String f_id = photo.stream()
+                .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                .findFirst()
+                .orElse(null).getFileId();
+        if(photo != null){
+            System.out.println(f_id);
+        }*/
+
+
+        /*final Document doc = update.getMessage().getDocument();
+        if(doc != null){
+            final String fileId = doc.getFileId();
+            final String fileName = doc.getFileName();
+
+            System.out.println(fileId);
+            System.out.println(fileName);
+        }*/
+
         Model model = new Model();
         Message message = update.getMessage();
         if(message != null && message.hasText() ){
@@ -78,6 +103,12 @@ public class Bot extends TelegramLongPollingBot {
 
                     break;
 
+                    default:
+                     try{
+                      sendMsg(message, Weather.getWeather(message.getText() , model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Такой город не найден");
+                    }
             }
 
         }
